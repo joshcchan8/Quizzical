2bb7676d5926600e926b9db892360f9afb29df81
joshc4 <joshchan4444@gmail.com>
2022-12-28 18:21:31 -0800

    added conditional styling, scoring & selection

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,126 @@
 import { useState, useEffect } from 'react'
 import Question from "./components/Question"
-import { nanoid } from "nanoid"
 import { decode } from 'html-entities'
+import Blob from "./assets/blobs.png"
+import Blob2 from "./assets/blobs2.png"
+import Blob3 from "./assets/blobs3.png"
+import Blob4 from "./assets/blobs4.png"
 
 export default function App() {
 
-  const [started, setStarted] = useState(false)
+  const [isHome, setIsHome] = useState(true)
   const [triviaData, setTriviaData] = useState([])
+  const [isSubmitted, setIsSubmitted] = useState(false)
+  const [score, setScore] = useState(0)
+  const [reset, setReset] = useState(false)
 
   // Starts the quiz
   function startQuiz() {
-    setStarted(true)
+    setIsHome(false)
   }
 
   useEffect(function() {
     fetch("https://opentdb.com/api.php?amount=5")
       .then(res => res.json())
-      .then(data => setTriviaData(data.results))
-  }, [])
+      .then(data => {
+        setTriviaData(data.results.map(item => {
+          const incorrect = decode(item.incorrect_answers)
+          return {
+            question: decode(item.question),
+            answer: decode(item.correct_answer),
+            options: randomizeAnswers([...incorrect, decode(item.correct_answer)]),
+            selected: ""
+          }
+        }))
+      })
+  }, [reset])
 
-  const questionElements = triviaData.map(data => {
+  // 5 questions in the quiz
+  const questionElements = triviaData.map((data, index) => {
     return (
-      <Question 
-        key={nanoid()}
-        question={decode(data.question)} 
-        answer={decode(data.correct_answer)} 
-        incorrect={decode(data.incorrect_answers)}
+      <Question
+        key={index}
+        question={data.question}
+        answer={data.answer}
+        options={data.options}
+        selected={data.selected}
+        selectOption={selectOption}
+        submitted={isSubmitted}
       />
     )
   })
 
-  return (
+  // Randomizes the order of the options, except for true and false
+  function randomizeAnswers(values){
 
-    <main className="main">
-      {!started ?
+    if ((values[0] == "True" && values[1] == "False") || 
+        (values[0] == "False" && values[1] == "True")) {
+        if (values[0] == "True") {
+            return values
+        } else {
+            return [values[1], values[0]]
+        }
+    }
+
+    let index = values.length;
+    let randomIndex;
+
+    // While there remain elements to shuffle.
+    while (index != 0) {
+
+        // Pick a remaining element.
+        randomIndex = Math.floor(Math.random() * index);
+        index--;
+    
+        // And swap it with the current element.
+        [values[index], values[randomIndex]] = [
+        values[randomIndex], values[index]];
+    }
+    return values;
+  }
+
+  // Updates array based on the selected option
+  function selectOption(question, selected) {
+    setTriviaData(prevTriviaData => prevTriviaData.map(item => {
+      return item.question === question ? 
+        {...item, selected: selected} :
+        item
+    }))
+  }
+
+  function checkQuiz() {
+    triviaData.map(item => {
+      if (item.selected === item.answer) {
+        setScore(prevCount => prevCount + 1)
+      }
+    })
+    setIsSubmitted(true)
+  }
+
+  function resetQuiz() {
+    setScore(0)
+    setIsSubmitted(false)
+    setReset(prevReset => !prevReset)
+  }
+
+  // Changes the background depending on whether the quiz has started
+  // TODO: fix this
+  const backgroundStyles = {
+    home: {
+      backgroundImage: `url(${Blob}), url(${Blob2})`,
+      backgroundRepeat: "no-repeat, no-repeat",
+      backgroundPosition: "right 0%, left 100%"
+    },
+    quiz: {
+      backgroundImage: `url(${Blob3}), url(${Blob4})`,
+      backgroundRepeat: "no-repeat, no-repeat",
+      backgroundPosition: "right 0%, left 100%"
+    }
+  }
+
+  return (
+    <main className="main" style={isHome ? backgroundStyles.home : backgroundStyles.quiz}>
+      {isHome ?
         <div className="home">
           <h2 className="home--title">Quizzical</h2>
           <h4 className="home--description">A true test of knowledge.</h4>
@@ -41,10 +128,13 @@ export default function App() {
         </div> :
         <div className="quiz">
           {questionElements}
-          <div>
-            <h4 className="quiz--score">You scored ___ correct answers</h4>
-            <button className="quiz--button">Play again</button>
-          </div>
+          {isSubmitted ? 
+            <div className="quiz--summary">
+              <h4 className="quiz--score">You scored {score}/5 correct answers</h4>
+              <button className="quiz--reset" onClick={resetQuiz}>Play again</button>
+            </div> : 
+            <button className="quiz--submit" onClick={checkQuiz}>Check Answers</button>
+          }
         </div>
       }
     </main>
--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -2,39 +2,115 @@ import React from 'react'
 
 export default function Question(props) {
 
-    const randomArray = randomizeAnswers()
+    const randomArray = props.options
 
-    // Randomizes the order of the
-    function randomizeAnswers(){
+    function select(selected) {
+        props.selectOption(props.question, selected)
+    }
 
-        let values = [...props.incorrect, props.answer]
-        let index = 4;
-        let randomIndex;
-  
-        // While there remain elements to shuffle.
-        while (index != 0) {
-    
-            // Pick a remaining element.
-            randomIndex = Math.floor(Math.random() * index);
-            index--;
-        
-            // And swap it with the current element.
-            [values[index], values[randomIndex]] = [
-            values[randomIndex], values[index]];
+    const styles = {
+        selected: {
+            backgroundColor: "#D6DBF5",
+            border: "1px solid #D6DBF5"
+        },
+        notSelected: {
+            backgroundColor: "transparent"
+        },
+        finished: {
+            opacity: "50%"
+        },
+        incorrect: {
+            backgroundColor: "#F8BCBC",
+            border: "1px solid #F8BCBC",
+            opacity: "50%"
+        },
+        correct: {
+            backgroundColor: "#94D7A2",
+            border: "1px solid #94D7A2"
         }
-        return values;
+    }
+
+    // Chooses the style for each button, based on the current state of the quiz
+    function chooseStyle(index) {
+        if (randomArray[index] === props.answer) {
+            if (props.submitted) {
+                return styles.correct
+            } else {
+                if (props.selected === randomArray[index]) {
+                    return styles.selected
+                } else {
+                    return styles.notSelected
+                }
+            }
+        } else {
+            if (props.submitted) {
+                if (props.selected === randomArray[index]) {
+                    return styles.incorrect
+                } else {
+                    return styles.finished
+                }
+            } else {
+                if (props.selected === randomArray[index]) {
+                    return styles.selected
+                } else {
+                    return styles.notSelected
+                }
+            }
+        }
     }
 
     return (
         <div className="question">
             <h3 className="question--prompt">{props.question}</h3>
-            <div className="question--options">
-                <button className="question--op1">{randomArray[0]}</button>
-                <button className="question--op2">{randomArray[1]}</button>
-                <button className="question--op3">{randomArray[2]}</button>
-                <button className="question--op4">{randomArray[3]}</button>
-            </div>
-            <hr></hr>
+            {randomArray.length != 2 ? 
+                <div className="question--options">
+                    <button
+                        style={chooseStyle(0)}
+                        onClick={() => select(randomArray[0])}
+                        disabled={props.submitted}
+                    >
+                        {randomArray[0]}
+                    </button>
+                    <button
+                        style={chooseStyle(1)}
+                        onClick={() => select(randomArray[1])}
+                        disabled={props.submitted}
+                    >
+                        {randomArray[1]}
+                    </button>
+                    <button
+                        style={chooseStyle(2)}
+                        onClick={() => select(randomArray[2])}
+                        disabled={props.submitted}
+                    >
+                        {randomArray[2]}
+                    </button>
+                    <button
+                        style={chooseStyle(3)}
+                        onClick={() => select(randomArray[3])}
+                        disabled={props.submitted}
+                    >
+                        {randomArray[3]}
+                    </button>
+                </div> :
+                <div className="question--options">
+                    <button
+                        style={chooseStyle(0)}
+                        onClick={() => select(randomArray[0])}
+                        disabled={props.submitted}
+                    >
+                        {randomArray[0]}
+                    </button>
+                    <button
+                        style={chooseStyle(1)}
+                        onClick={() => select(randomArray[1])}
+                        disabled={props.submitted}
+                    >
+                        {randomArray[1]}
+                    </button>
+                </div>
+            }
+            <hr question--divider></hr>
         </div>
     )
 }
